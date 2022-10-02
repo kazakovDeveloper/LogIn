9100bc3a88d20ea35b6cd0f127f4f27d32cd05bb
Kazakov Danil <kazakov777@internet.ru>
2022-10-02 23:59:15 +0300

    Second commit

--- LogInApp/ViewController.swift
+++ LogInApp/ViewController.swift
@@ -51,13 +51,28 @@ class ViewController: UIViewController {
         dismiss(animated: true)
     }
     
+    func setGradientBackground() {
+        let colorTop =  UIColor(red: 255.0/255.0, green: 149.0/255.0, blue: 0.0/255.0, alpha: 1.0).cgColor
+        let colorBottom = UIColor(red: 255.0/255.0, green: 94.0/255.0, blue: 58.0/255.0, alpha: 1.0).cgColor
+                    
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.colors = [colorTop, colorBottom]
+        gradientLayer.locations = [0.0, 1.0]
+        gradientLayer.frame = self.view.bounds
+                
+        self.view.layer.insertSublayer(gradientLayer, at:0)
+    }
     
+    override func viewWillAppear(_ animated: Bool) {
+        setGradientBackground()
+        super.viewWillAppear(animated)
+    }
     
     @IBAction func LogInActionButton() {
         if loginOutlet.text == login && passwordOutlet.text == password {
             performSegue(withIdentifier: "goToSecondScreen", sender: nil)
         } else {
-            let alert = UIAlertController(title: "Incorrect password or login", message: login, preferredStyle: UIAlertController.Style.alert)
+            let alert = UIAlertController(title: "error enter", message: "Your login or password is incorrect", preferredStyle: UIAlertController.Style.alert)
             
             alert.addAction(UIAlertAction(title: "Again", style: UIAlertAction.Style.default, handler: nil))
             
